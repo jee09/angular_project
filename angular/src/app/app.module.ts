edcea0ce33c9687f228f88359eb6b16e5c907bf5
saha <jungwonji1960@gmail.com>
2021-06-24 18:33:42 +0900

    [0625][#00000-로그인] 로그인 기능 추가 - form 세팅

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,20 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list'
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'
+import {LoginComponent} from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatDividerModule} from "@angular/material/divider";
+import {SidenavComponent} from './sidenav/sidenav.component';
+import {HttpClientModule, HttpClientJsonpModule} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormGroup, FormBuilder} from '@angular/forms';
+import {Inject} from "@angular/core";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
 
+  // form: FormGroup;
+  //
+  // constructor(@Inject(FormBuilder) formBuilder: FormBuilder,
+  //             private router: Router,
+  //             private route: ActivatedRoute,
+  //             public _location: Location)
+  //
+  // {
+  //   this.form = formBuilder.group({
+  //     loginID: [''],
+  //     loginPW: ['']
+  //   });
+  //   let id = this.form.value["loginID"];
+  //   console.log(id);
+  //
+  // }
+  //
+  // Login() : void
+  // {
+  //   let id = this.form.value["loginID"];
+  //   let pw = this.form.value["loginPW"];
+  //   console.log(id + ", " + pw);
+  // }
 }
